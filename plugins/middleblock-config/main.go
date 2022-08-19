ae584a51f60d548cdbc40bf83bbf604209e94d8b
adibrastegarnia <adib.rastegarnia@intel.com>
2022-08-19 12:24:46 -0700

    middlebloc-with-config

---
+++ plugins/middleblock-config/main.go
@@ -0,0 +1,21 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package main
+
+import (
+	_ "embed"
+	"github.com/onosproject/onos-p4-plugins/plugins/middleblock-config/plugin"
+	"os"
+)
+
+// P4Plugin p4 plugin instance
+var P4Plugin plugin.P4Plugin
+
+func init() {
+	err := P4Plugin.InitPlugin()
+	if err != nil {
+		os.Exit(1)
+	}
+}
---
+++ plugins/middleblock-config/plugin/plugin.go
@@ -0,0 +1,59 @@
+// SPDX-FileCopyrightText: 2022-present Intel Corporation
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package plugin
+
+import (
+	_ "embed"
+	"github.com/onosproject/onos-lib-go/pkg/errors"
+	"github.com/onosproject/onos-lib-go/pkg/logging"
+	p4configapi "github.com/p4lang/p4runtime/go/p4/config/v1"
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+var log = logging.GetLogger()
+
+//go:embed artifacts/p4info.txt
+var p4InfoBytes []byte
+
+////go:embed artifacts/middleblock_bmv2.json
+var p4DeviceConfig []byte
+
+// P4Plugin p4 plugin struct
+type P4Plugin struct {
+	p4Info         *p4configapi.P4Info
+	p4DeviceConfig []byte
+}
+
+// InitPlugin initialize P4 plugin using P4Info and P4 device config
+func (p *P4Plugin) InitPlugin() error {
+	p4Info := &p4configapi.P4Info{}
+	if err := prototext.Unmarshal(p4InfoBytes, p4Info); err != nil {
+		return errors.NewInvalid("failed to decode P4Info Protobuf message", err)
+	}
+	p.p4Info = p4Info
+	p.p4DeviceConfig = p4DeviceConfig
+	return nil
+}
+
+// GetP4Info returns P4 info
+func (p *P4Plugin) GetP4Info() (*p4configapi.P4Info, error) {
+	return p.p4Info, nil
+}
+
+// GetPkgInfo returns P4 Pkg info
+func (p *P4Plugin) GetPkgInfo() (*p4configapi.PkgInfo, error) {
+	if p.p4Info != nil {
+		log.Debug("Getting package info")
+		return p.p4Info.PkgInfo, nil
+
+	}
+	return nil, errors.NewNotFound("Package info is not initialized")
+}
+
+// GetP4DeviceConfig returns P4 device config
+func (p *P4Plugin) GetP4DeviceConfig() ([]byte, error) {
+	log.Debug("Getting P4 Device config")
+	return p.p4DeviceConfig, nil
+}
